d1fa560e82abce4652efd4409ab8faf1bdd2718c
Austin Hester <ahester@incomm.com>
2020-07-02 16:00:28 -0500

    downcase + filter non-alpha
    Signed-off-by: Austin Hester <ahester@incomm.com>

--- Hester_Project1.rb
+++ Hester_Project1.rb
@@ -1,7 +1,9 @@
 # Hester_Project1.rb
+# ruby 2.6.6p146 (2020-03-31 revision 67876) [x64-mingw32]
+
 require_relative 'Vigenere.rb'
 
-v = Vigenere.new("dogs are good");
+v = Vigenere.new("Dogs ar4e Good!2");
 puts(v.encrypt("cat"))
 puts(v.decrypt_with_key("cat"))
 puts(v.decrypt_with_known_plain_text_key_length("dogs", 3))
--- Vigenere.rb
+++ Vigenere.rb
@@ -1,4 +1,6 @@
 # Vigenere.rb
+# ruby 2.6.6p146 (2020-03-31 revision 67876) [x64-mingw32]
+
 class Vigenere
     attr_accessor(:key, :plain_text)
 
@@ -8,14 +10,19 @@ class Vigenere
     end
 
     def encrypt(key)
-        @cipher_text = plain_text.upcase
+        @cipher_text = plain_text.downcase.chars.reject { |x| !letter?(x) }
     end
 
     def decrypt_with_key(key)
-        @cipher_text.downcase
+        @cipher_text.join('')
     end
 
     def decrypt_with_known_plain_text_key_length(substring, key_length)
         "TBD"
+    end
+
+    # https://stackoverflow.com/questions/14551256/ruby-how-to-find-out-if-a-character-is-a-letter-or-a-digit
+    def letter?(lookAhead)
+        lookAhead =~ /[[:alpha:]]/
     end
 end
